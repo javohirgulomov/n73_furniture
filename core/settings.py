b3637cedd9eb716ee8eed148c601acb9402d78fa
admin <admin@email>
2026-02-16 11:35:55 +0500

    first commit

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'o^4pz3*b^=m&l50*g^mfi)jn2^19maj8_86r%x&ng*(yb(53l7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
--- core/urls.py
+++ core/urls.py
@@ -19,9 +19,48 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+from django.urls import path
+
+from shared import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('shared.urls', namespace='shared')),
+    # Home pages
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('home2/', views.home2, name='home2'),
+    path('home3/', views.home3, name='home3'),
+    path('home4/', views.home4, name='home4'),
+    path('home5/', views.home5, name='home5'),
+
+    # Blog pages
+    path('blogs-list/', views.blogs_list, name='blogs-list'),
+    path('blog-list-sidebar-left2/', views.blog_list_sidebar_left2, name='blog-list-sidebar-left2'),
+    path('blog-list-sidebar-right/', views.blog_list_sidebar_right, name='blog-list-sidebar-right'),
+    path('blog-list-no-sidebar/', views.blog_list_no_sidebar, name='blog-list-no-sidebar'),
+    path('blog-grid-no-sidebar/', views.blog_grid_no_sidebar, name='blog-grid-no-sidebar'),
+    path('blog-detail/', views.blog_detail, name='blog-detail'),
+
+    # Product pages
+    path('products-list/', views.products_list, name='products-list'),
+    path('product-grid-sidebar-right/', views.product_grid_sidebar_right, name='product-grid-sidebar-right'),
+    path('product-list-sidebar-left/', views.product_list_sidebar_left, name='product-list-sidebar-left'),
+    path('product-detail/', views.product_detail, name='product-detail'),
+
+    # User account pages
+    path('account/', views.account, name='account'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+
+    # Shopping pages
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+
+    # Other pages
+    path('contact/', views.contact, name='contact'),
+    path('about-us/', views.about_us, name='about-us'),
+    path('404/', views.page_404, name='404'),
 ]
 
 if settings.DEBUG:
