15ce8b5a58edc21d0e023caffe8c696e73409332
admin <admin@email>
2026-03-11 18:43:16 +0500

    done

--- blogs/views.py
+++ blogs/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
-
 from blogs.models import Blog, BlogStatus, Category, Tag
 
 
 def blogs_list_view(request):
+    blogs = Blog.objects.filter(status=BlogStatus.PUBLISHED)
+
+    # Get filter values from URL
+    category_id = request.GET.get('category')
+    tag_id = request.GET.get('tag')
+
+    # Apply filters if selected
+    if category_id:
+        blogs = blogs.filter(categories__id=category_id)
+
+    if tag_id:
+        blogs = blogs.filter(tags__id=tag_id)
+
     context = {
-        "blogs": Blog.objects.filter(status=BlogStatus.PUBLISHED),
+        "blogs": blogs,
         "categories": Category.objects.filter(parent=None),
         "tags": Tag.objects.all(),
-        "recent_posts": Blog.objects.order_by('-created_at')[:2]
+        "recent_posts": Blog.objects.filter(status=BlogStatus.PUBLISHED).order_by('-created_at')[:2],
+        "selected_category": category_id,
+        "selected_tag": tag_id,
     }
-    return render(
-        request, 'blogs/blogs-list.html',
-        context
-    )
+    return render(request, 'blogs/blogs-list.html', context)
 
 
 def blog_detail_view(request, pk):
@@ -25,7 +36,4 @@ def blog_detail_view(request, pk):
     context = {
         "blog": blog
     }
-    return render(
-        request, 'blogs/blog-detail.html',
-        context
-    )
+    return render(request, 'blogs/blog-detail.html', context)
--- products/views.py
+++ products/views.py
@@ -1,27 +1,50 @@
 from django.shortcuts import render
-
-from products.models import Product, ProductStatus, ProductCategory, ProductTag
+from products.models import Product, ProductCategory, ProductTag, ProductColor, Manufacture
 
 
 def products_list_view(request):
+    products = Product.objects.filter(is_active=True)
+
+    # Get filter values from URL
+    category_id = request.GET.get('category')
+    tag_id = request.GET.get('tag')
+    color_id = request.GET.get('color')
+    manufacture_id = request.GET.get('manufacture')
+
+    # Apply filters if selected
+    if category_id:
+        products = products.filter(categories__id=category_id)
+
+    if tag_id:
+        products = products.filter(tags__id=tag_id)
+
+    if color_id:
+        products = products.filter(colors__id=color_id)
+
+    if manufacture_id:
+        products = products.filter(manufacture__id=manufacture_id)
+
     context = {
-        "products": Product.objects.filter(status=ProductStatus.AVAILABLE, is_active=True),
-        "categories": ProductCategory.objects.filter(parent=None, is_active=True),
-        "tags":ProductTag.objects.all(),
+        "products": products,
+        "categories": ProductCategory.objects.filter(is_active=True),
+        "tags": ProductTag.objects.all(),
+        "colors": ProductColor.objects.all(),
+        "manufactures": Manufacture.objects.filter(is_active=True),
+        "selected_category": category_id,
+        "selected_tag": tag_id,
+        "selected_color": color_id,
+        "selected_manufacture": manufacture_id,
     }
-    return render(request, 'products/products-list.html', context)
+    return render(request, 'products/products-list.html', context=context)
 
 
 def products_detail_view(request, pk):
     try:
-        product = Product.objects.get(id=pk, is_active=True)
+        product = Product.objects.get(pk=pk, is_active=True)
     except Product.DoesNotExist:
         return render(request, 'shared/404.html')
+
     context = {
-        "product": product,
+        "product": product
     }
-    return render(request, 'products/product-detail.html', context)
-
-
-
-
+    return render(request, 'products/product-detail.html', context=context)
